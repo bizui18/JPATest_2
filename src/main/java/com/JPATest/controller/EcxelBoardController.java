d473e57ee4aaf25fb192e9b1e45e9273d996540b
bexelpatra <83278536+bexelpatra@users.noreply.github.com>
2023-06-23 16:21:08 +0900

    cover page borderline updated

--- src/main/java/com/JPATest/controller/EcxelBoardController.java
+++ src/main/java/com/JPATest/controller/EcxelBoardController.java
@@ -16,10 +16,13 @@ import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.poi.openxml4j.util.ZipSecureFile;
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.Row.MissingCellPolicy;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -42,33 +45,36 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableAutoConfiguration
 @RequestMapping(value = "/views")
 public class EcxelBoardController implements WebMvcConfigurer {
-	
+
 	private final static Logger logger = LoggerFactory.getLogger("");
-	//로컬용
-	//private String OPERATE_PATH = System.getProperty("user.dir") + "/src/main/webapp/dailyReport/";
-	//private String FINISH_PATH = System.getProperty("user.dir") + "/src/main/webapp/dailyReportFin/";
-	
-	//배포용
+	// 로컬용
+	// private String OPERATE_PATH = System.getProperty("user.dir") +
+	// "/src/main/webapp/dailyReport/";
+	// private String FINISH_PATH = System.getProperty("user.dir") +
+	// "/src/main/webapp/dailyReportFin/";
+
+	// 배포용
 	private String OPERATE_PATH = "/dailyReport/";
 	private String FINISH_PATH = "/dailyReportFin/";
 
 	// 서버 배포 시 주석 해제해야 됨(파일 다운로드를 위해)
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/dailyReportFin/**")
-                .addResourceLocations("file:///dailyReportFin/");
-    }
-	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/dailyReportFin/**")
+				.addResourceLocations("file:///dailyReportFin/");
+	}
+
 	@RequestMapping("/dailyReportExcel")
-	public String dailyReportExcel(@RequestParam(value="schDrwNo", required=false) String schDrwNo, Model model) throws Exception {
+	public String dailyReportExcel(@RequestParam(value = "schDrwNo", required = false) String schDrwNo, Model model)
+			throws Exception {
 		logger.info("###### START [EcxelBoardController :: /views/dailyReportExcel] ######");
-		
+
 		makeFolder(OPERATE_PATH);
 		makeFolder(FINISH_PATH);
-		
+
 		deleteFile(OPERATE_PATH);
-		
-        logger.info("###### END [EcxelBoardController :: /views/dailyReportExcel] ######");
+
+		logger.info("###### END [EcxelBoardController :: /views/dailyReportExcel] ######");
 		return "dailyReportExcel";
 	}
 
@@ -76,16 +82,16 @@ public class EcxelBoardController implements WebMvcConfigurer {
 	@PostMapping("/multiUpload")
 	public String multiUpload(MultipartFile[] uploadFile, String text) throws Exception {
 		logger.info("###### START [EcxelBoardController :: /views/multiUpload] ######");
-		
+
 		String resutlMsg = "";
 		String uploadFolder = OPERATE_PATH + text + "/";
 		logger.info("멀티 업로드 파일경로 : " + uploadFolder);
-		
+
 		makeFolder(uploadFolder);
-		
-		for(MultipartFile multipartFile : uploadFile) {
+
+		for (MultipartFile multipartFile : uploadFile) {
 			logger.info("멀티 업로드 파일명 : " + multipartFile.getOriginalFilename());
-			
+
 			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
 			try {
 				multipartFile.transferTo(saveFile);
@@ -93,9 +99,9 @@ public class EcxelBoardController implements WebMvcConfigurer {
 				logger.error(e.getMessage());
 			}
 		}
-		
+
 		resutlMsg = excelOperate(text);
-		
+
 		logger.info("###### END [EcxelBoardController :: /views/multiUpload] ######");
 		return resutlMsg;
 	}
@@ -104,7 +110,7 @@ public class EcxelBoardController implements WebMvcConfigurer {
 	@RequestMapping("/excelOperate")
 	public String excelOperate(@RequestParam String text) throws Exception {
 		logger.info("###### START [EcxelBoardController :: /views/excelOperate] ######");
-		
+
 		String directo = OPERATE_PATH + text + "/";
 		logger.info("엑셀작업 경로 : " + directo);
 
@@ -112,48 +118,48 @@ public class EcxelBoardController implements WebMvcConfigurer {
 
 		for (Telecom TEL : Telecom.values()) {
 			// 통신사별 폴더 생성
-			File f = new File(directo + TEL.getName()+"/");
-			
-			if(!f.exists()) {
+			File f = new File(directo + TEL.getName() + "/");
+
+			if (!f.exists()) {
 				f.mkdirs();
 			}
 
 			// 파일 복사
 			for (File file : new File(directo).listFiles()) {
-				if(file.isFile()) {					
+				if (file.isFile()) {
 					copyFile(file, f);
 				}
 			}
 
 			// 엑셀 수정
 			logger.info("[작업 시작] : " + TEL.getName());
-			
-			exel1(f.getAbsolutePath(),TEL);
-			
-			for (File file : f.listFiles()) {				
-				if(file.isFile()) {
+
+			exel1(f.getAbsolutePath(), TEL);
+
+			for (File file : f.listFiles()) {
+				if (file.isFile()) {
 					for (File oriFile : new File(directo).listFiles()) {
-						if(file.getName().equals(oriFile.getName())) {
+						if (file.getName().equals(oriFile.getName())) {
 							file.delete();
 							System.out.println("check");
 						}
-					}	
+					}
 				}
 			}
 		}
-		logger.info("작업 시간 : " + (System.currentTimeMillis() - start)/1000);
-    	
+		logger.info("작업 시간 : " + (System.currentTimeMillis() - start) / 1000);
+
 		try {
-	    	Date today = new Date();
-	    	SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");         
-			
+			Date today = new Date();
+			SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+
 			compress(directo, FINISH_PATH, df.format(today).toString() + "_" + text);
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
-		
+
 		deleteFile(OPERATE_PATH);
-		
+
 		logger.info("###### END [EcxelBoardController :: /views/excelOperate] ######");
 		return "작업이 완료되었습니다.";
 	}
@@ -162,9 +168,9 @@ public class EcxelBoardController implements WebMvcConfigurer {
 	@RequestMapping("/excelList")
 	public List<Map<String, String>> excelList(@RequestParam Map<String, Object> map) throws Exception {
 		logger.info("###### START [EcxelBoardController :: /views/excelList] ######");
-		
+
 		List<Map<String, String>> list = new ArrayList<>();
-		
+
 		File dir = new File(FINISH_PATH);
 
 		String[] filenames = dir.list();
@@ -174,81 +180,86 @@ public class EcxelBoardController implements WebMvcConfigurer {
 			rstMap.put("path", FINISH_PATH);
 			list.add(rstMap);
 		}
-		
+
 		logger.info("###### END [EcxelBoardController :: /views/excelList] ######");
 		return list;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping("/delExcel")
 	public String delExcel(@RequestParam String fileNm) throws Exception {
 		logger.info("###### START [EcxelBoardController :: /views/delExcel] ######");
 		logger.info("삭제 파일명 : " + fileNm);
-		
+
 		String resultMsg = "";
-		
+
 		File file = new File(FINISH_PATH + fileNm);
-		
-		if(file.exists()) {
-			if(file.delete()) { 
+
+		if (file.exists()) {
+			if (file.delete()) {
 				resultMsg = "파일 삭제";
-			}else{
+			} else {
 				resultMsg = "파일 삭제 실패";
 			}
-		}else {
+		} else {
 			resultMsg = "파일이 존재하지 않습니다.";
 		}
-		
+
 		logger.info("###### END [EcxelBoardController :: /views/delExcel] ######");
 		return resultMsg;
 	}
-	
 
 	private static void exel1(String directo, Telecom TEL) throws IOException, FileNotFoundException {
 		File dir = new File(directo);
 		File[] file = dir.listFiles();
-		
-		ZipSecureFile.setMinInflateRatio(0);		
-		
+
+		ZipSecureFile.setMinInflateRatio(0);
+
 		FileOutputStream os = null;
 		XSSFWorkbook x = null;
-		
+
 		for (File src : file) {
-			if(src.isDirectory()) {
+			if (src.isDirectory()) {
 				continue;
 			}
-			if(!src.getName().endsWith("xlsx")) {
+			if (!src.getName().endsWith("xlsx")) {
 				continue;
 			}
-			
+
 			x = (XSSFWorkbook) WorkbookFactory.create(src);
+
 			
-			XSSFSheet shit = x.getSheetAt(1);
+			coverShitJobs(x);
 			
+			/**
+			 * main shit 수정
+			 */
+			XSSFSheet mainShit = x.getSheetAt(1);
+
 			// sk 머지된 영역 제거하기
 			// 한번만 반복하면 풀리지 않는 머지 영역이 있어서 3번을 돌린다.
-			for (int j = 0; j < 3; j++) {				
-				for (int i = 0; i < shit.getNumMergedRegions(); i++) {
-					CellRangeAddress s = shit.getMergedRegion(i);
-					if(15 < s.getFirstRow() && s.getFirstRow() < 26) {
-						shit.removeMergedRegion(i);
+			for (int j = 0; j < 3; j++) {
+				for (int i = 0; i < mainShit.getNumMergedRegions(); i++) {
+					CellRangeAddress s = mainShit.getMergedRegion(i);
+					if (15 < s.getFirstRow() && s.getFirstRow() < 26) {
+						mainShit.removeMergedRegion(i);
 					}
 				}
 			}
-			
+
 			// 상단 2줄에 값 옮겨 넣기
-			shit.getRow(11).getCell(1).setCellValue(TEL.getName());
-			copyRowValue(shit.getRow(13),shit.getRow(13+TEL.getRow()),0);
-			copyRowValue(shit.getRow(14),shit.getRow(14+TEL.getRow()),0);
+			mainShit.getRow(11).getCell(1).setCellValue(TEL.getName());
+			copyRowValue(mainShit.getRow(13), mainShit.getRow(13 + TEL.getRow()), 0);
+			copyRowValue(mainShit.getRow(14), mainShit.getRow(14 + TEL.getRow()), 0);
 
 			// 나머지 줄 삭제
 			for (int i = 16; i < 26; i++) {
-				shit.createRow(i);
+				mainShit.createRow(i);
 			}
-			
+
 			// 줄별 처리
 			for (int i = 31; i < 37; i++) { // row
-				XSSFRow ro = shit.getRow(i);
+				XSSFRow ro = mainShit.getRow(i);
 				if (ro == null)
 					continue;
 
@@ -268,16 +279,17 @@ public class EcxelBoardController implements WebMvcConfigurer {
 					}
 				}
 			}
-			
+
 			// 빈줄 잘라내기
-			shit.shiftRows(26, 48, -10);
-			
+			mainShit.shiftRows(26, 48, -10);
+
 			// 마무리 작업
 			File newFolder = new File(directo);
-			if(!newFolder.exists()) {
+			if (!newFolder.exists()) {
 				newFolder.mkdirs();
 			}
-			os = new FileOutputStream(new File(newFolder.getAbsolutePath()+"/"+TEL.getName()+"_"+src.getName()));
+			os = new FileOutputStream(
+					new File(newFolder.getAbsolutePath() + "/" + TEL.getTitle() + "_" + src.getName()));
 			x.write(os);
 
 			x.close();
@@ -286,52 +298,84 @@ public class EcxelBoardController implements WebMvcConfigurer {
 		}
 	}
 
-	private static void copyRowValue(XSSFRow origin,XSSFRow src,int start) {
+	// 표지 줄 테두리 설정하기
+	private static XSSFSheet coverShitJobs(XSSFWorkbook x) {
+		XSSFSheet coverShit = x.getSheetAt(0);
+		BorderStyle top = BorderStyle.NONE;
+		BorderStyle bottom = BorderStyle.HAIR;
+		int firstRow = 21;
+		int lastRow = firstRow + 3;
+		int firstCell = 4;
+		int lastCell = firstCell+ 4;
+		XSSFRow ro = null;
+		for (int i = firstRow; i < lastRow + 1; i++) {
+			ro = coverShit.getRow(i);
+			
+			if (i==lastRow) {
+				top = BorderStyle.THIN;
+				bottom = BorderStyle.NONE;
+				System.out.printf("top : %s , bottom : %s\n",top.name(),bottom.name());
+			}
+			
+			for (int j = firstCell; j <lastCell ; j++) {
+				XSSFCell c = ro.getCell(j);
+				XSSFCellStyle cellStyle = c.getCellStyle();
+				cellStyle.setBorderTop(top);
+				cellStyle.setBorderBottom(bottom);
+				c.setCellStyle(cellStyle);
+			}
+			
+		}
+		
+		return coverShit;
+	}
+
+	private static void copyRowValue(XSSFRow origin, XSSFRow src, int start) {
 		for (int j = start; j < origin.getLastCellNum(); j++) {
 			XSSFCell t = origin.getCell(j);
 			XSSFCell s = src.getCell(j);
-			if(t!=null && s !=null) {				
+			if (t != null && s != null) {
 				copyCellValue(t, s);
 			}
 		}
 	}
-	
+
 	private static void copyCellValue(XSSFCell origin, XSSFCell src) {
 		CellType type = origin.getCellType();
-		if(type == CellType.STRING) {
+		if (type == CellType.STRING) {
 			origin.setCellValue(src.getStringCellValue());
-		}else if(type==CellType.NUMERIC) {
+		} else if (type == CellType.NUMERIC) {
 			origin.setCellValue(src.getNumericCellValue());
-		}else if(type==CellType.FORMULA) {
-			origin.setCellValue(src.getCellFormula());			
-		}else {
+		} else if (type == CellType.FORMULA) {
+			origin.setCellValue(src.getCellFormula());
+		} else {
 			origin.setCellValue(src.getRawValue());
 		}
 	}
-	
+
 	private static void copyFile(File origin, File... toDiretory) throws Exception {
 
 		// 1. parameter check
 		// 1-1 file 인지 확인
-		if(!origin.isFile()) {
+		if (!origin.isFile()) {
 			logger.info("origin file must be file : " + origin.getName());
 			return;
 		}
 		// 1-2 directory인지 확인
 		for (File file : toDiretory) {
-			if(!file.isDirectory() || file == null) {
+			if (!file.isDirectory() || file == null) {
 				logger.info("where you want to transfer is not directory");
 				return;
 			}
 		}
-		
+
 		// 2. FileInputStream, FileOutputStream 준비
 		FileInputStream input = new FileInputStream(origin);
 		FileOutputStream[] oss = new FileOutputStream[toDiretory.length];
 		for (int i = 0; i < oss.length; i++) {
-			oss[i] = new FileOutputStream(toDiretory[i].getAbsolutePath()+"/"+origin.getName());
+			oss[i] = new FileOutputStream(toDiretory[i].getAbsolutePath() + "/" + origin.getName());
 		}
-	
+
 		// 3. 한번에 read하고, write할 사이즈 지정
 		byte[] buf = new byte[1024];
 
@@ -352,20 +396,26 @@ public class EcxelBoardController implements WebMvcConfigurer {
 	
 	public enum Telecom {
 
-		SKT("SKT",0,0),
-		KT("KT",5,2),
-		LGU("LGU+",10,4),
+		SKT("SKT","SKT", 0, 0),
+		KT("KT", "KT", 5, 2),
+		LGU("LGU+", "LG U+", 10, 4),
 		;
-		private String name;
+		// LGU+는 파일 이름으로 사용하고 LG U+는 파일 내부에서 사용된다.
+		private String title; // 파일 이름에 사용되는 이름
+		private String name; // 파일 내부에서 사용되는 이름
 		private int row;
 		private int column;
-		
-		private Telecom(String name, int row, int column) {
+
+		private Telecom(String title,String name, int row, int column) {
+			this.title= title;
 			this.name = name;
 			this.row = row;
 			this.column = column;
 		}
 
+		public String getTitle() {
+			return title;
+		}
 		public String getName() {
 			return name;
 		}
@@ -378,55 +428,52 @@ public class EcxelBoardController implements WebMvcConfigurer {
 			return column;
 		}
 	}
-	
-	
-	
+
 	public static void makeFolder(String path) {
 		File Folder = new File(path);
 		if (!Folder.exists()) {
-			try{
+			try {
 				Folder.mkdirs();
 				logger.info("폴더가 생성되었습니다.");
-			} 
-			catch(Exception e){
+			} catch (Exception e) {
 				e.getStackTrace();
-			}        
+			}
 		}
 	}
-	
+
 	private void compress(String path, String outputPath, String outputFileName) throws Throwable {
-		// 파일 압축 성공 여부 
-		//boolean isChk = false;
-		
+		// 파일 압축 성공 여부
+		// boolean isChk = false;
+
 		File file = new File(path);
-		
-		// 파일의 .zip이 없는 경우, .zip 을 붙여준다. 
+
+		// 파일의 .zip이 없는 경우, .zip 을 붙여준다.
 		int pos = outputFileName.lastIndexOf(".") == -1 ? outputFileName.length() : outputFileName.lastIndexOf(".");
-		
-		// outputFileName .zip이 없는 경우 
+
+		// outputFileName .zip이 없는 경우
 		if (!outputFileName.substring(pos).equalsIgnoreCase(".zip")) {
 			outputFileName += ".zip";
 		}
-		
+
 		// 압축 경로 체크
 		if (!file.exists()) {
 			throw new Exception("Not File!");
 		}
-		
+
 		// 출력 스트림
 		FileOutputStream fos = null;
 		// 압축 스트림
 		ZipOutputStream zos = null;
-		
+
 		try {
 			fos = new FileOutputStream(new File(outputPath + outputFileName));
 			zos = new ZipOutputStream(fos);
-			
-			// 디렉토리 검색를 통한 하위 파일과 폴더 검색 
+
+			// 디렉토리 검색를 통한 하위 파일과 폴더 검색
 			searchDirectory(file, file.getPath(), zos);
-			
+
 			// 압축 성공.
-			//isChk = true;
+			// isChk = true;
 			logger.info("압축 성공");
 		} catch (Throwable e) {
 			throw e;
@@ -437,7 +484,7 @@ public class EcxelBoardController implements WebMvcConfigurer {
 				fos.close();
 		}
 	}
-	
+
 	/**
 	 * @description 디렉토리 탐색
 	 * @param file 현재 파일
@@ -465,7 +512,7 @@ public class EcxelBoardController implements WebMvcConfigurer {
 	}
 
 	/**
-	 * @description압축 메소드 
+	 * @description압축 메소드
 	 * @param file
 	 * @param root
 	 * @param zos
@@ -498,10 +545,22 @@ public class EcxelBoardController implements WebMvcConfigurer {
 	}
 
 	public static void deleteFile(String path) throws IOException {
-	    File directory = new File(path);
-	    FileUtils.cleanDirectory(directory);
-	    
-	    logger.info("작업 파일 삭제완료");
+		File directory = new File(path);
+		FileUtils.cleanDirectory(directory);
+
+		logger.info("작업 파일 삭제완료");
 	}
-	
+	public static void main(String[] args) {
+		System.out.println("시작");
+		String directo="D:\\t";
+		try {
+			exel1(directo, Telecom.LGU);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
